f03ea344dfdacd96bb88aedffeaa05b39e51dd9a
Masum Shahariar <100153961+XRC007@users.noreply.github.com>
2026-02-15 18:16:15 +0600

    Create Start.sh

---
+++ Start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Get number of CPU cores
+CORES=$(nproc)
+
+# Run SheepIt (Railway will provide environment variables)
+java -jar /app/sheepit.jar \
+  -login "${SHEEPIT_LOGIN}" \
+  -password "${SHEEPIT_PASSWORD}" \
+  -ui oneLine \
+  -compute-method CPU \
+  -cores ${CORES}
